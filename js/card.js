cba3311b5451c9b35298b03c832b7981995ac61d
Jay <sinaelj@gmail.com>
2023-05-24 00:53:20 +0900

    ♻️ 코드 유지관리 및 개선

--- js/card.js
+++ js/card.js
@@ -1,8 +1,21 @@
-const $gameTimer = document.querySelector('.game-timer');
-const $gameBoard = document.querySelector('.game-board');
-const $dialog = document.querySelector('#dialog-default')
-const $restart = document.querySelector('.restart');
+// Game Constants
+const gameElements = {
+    gameTimer: document.querySelector('.game-timer'),
+    gameBoard: document.querySelector('.game-board'),
+    dialog: document.querySelector('#dialog-default'),
+    restart: document.querySelector('.restart'),
+};
 
+let gameStatus = {
+    clickPause: false,
+    matchedCount: 0,
+    cards: null,
+    cardOne: null,
+    cardTwo: null,
+    time: null
+};
+
+// ? 두 가지 요소를 비교하는 방법으로 item 객체를 만드는 게 최선일까?
 let item = [
     { id: 1, idx: "1" },
     { id: 2, idx: "2" },
@@ -18,15 +31,7 @@ let item = [
     { id: 12, idx: "0" },
 ]
 
-// 카드 선택
-let clickPause = false;
-let matchedCount = 0;
-let time
-let cards
-let cardOne, cardTwo;
-
-// 게임 결과
-let resultObj = {
+let gameResults = {
     cleared: {
         msg: 'Cleared',
         img: 'result-win.gif',
@@ -38,10 +43,14 @@ let resultObj = {
         sound: new Audio('../asset/sound/lose.wav')
     },
     playSound: function (result) {
-        this.resultSound[result].play();
+        this[result].play();
     }
 };
-let { cleared, failed } = resultObj;
+
+// 게임 상태 구조 분해 할당
+let { cleared, failed } = gameResults;
+let { gameTimer, gameBoard, dialog, restart } = gameElements;
+let { clickPause, matchedCount, cards, cardOne, cardTwo, time } = gameStatus;
 
 // 게임 사운드
 const start = new Audio('../asset/sound/start.wav');
@@ -56,7 +65,7 @@ const start = new Audio('../asset/sound/start.wav');
  * @description item 객체의 순서를 변경해서 0 이상 1 미만의 부동소수점 난수 값을 반환.
  * @description -0.5는 반환되는 난수의 중간 값, .sort() 메서드로 매번 랜덤 한 값으로 정렬시켜 섞어준다.
  */
-const randomCard = () => {
+const shuffleCards = () => {
     item.sort(() => Math.random() - 0.5);
 }
 
@@ -67,9 +76,11 @@ const randomCard = () => {
  * @param {Array} item 카드 배열
  * @description 카드를 순회하면서 카드를 생성하고, 카드의 앞면과 뒷면을 생성하고  $gameBoard에 추가.
  */
-const setCard = () => {
+const renderCards = () => {
     // 초기화
-    $gameBoard.innerHTML = '';
+    gameBoard.innerHTML = '';
+
+    // 카드 생성
     for (let i = 0; i < item.length; i++) {
         const $createCard = document.createElement('div');
         const $front = document.createElement('div');
@@ -84,10 +95,13 @@ const setCard = () => {
         $back.classList.add('back');
         $back.innerText = '?';
 
-        $gameBoard.append($createCard);
+        gameBoard.append($createCard);
         $createCard.append($front, $back);
+
+        addCardClickEvent();
     }
 };
+
 
 
 /**
@@ -95,7 +109,7 @@ const setCard = () => {
  * @param {Array} cards 카드 배열
  * @description 카드를 순회하면서 카드 앞면을 보여하고 2초 후 뒷면으로 뒤집기.
  */
-const previewCard = () => {
+const showCards = () => {
     cards = document.querySelectorAll('.card');
     // 카드를 순회하면서 show class 추가
     cards.forEach((card, index) => {
@@ -112,15 +126,16 @@ const previewCard = () => {
 };
 
 
+
 /**
  * 4. 카운트다운 타이머
  * @description 30초 카운트다운 후 clearInterval() 메서드로 타이머 제거 후 게임 재시작 팝업창 띄우기
  */
-const timer = () => {
+const startTimer = () => {
     let count = 30;
     time = setInterval(() => {
         count--;
-        $gameTimer.innerText = count;
+        gameTimer.innerText = count;
         if (count === 0) {
             endGame(failed);
         }
@@ -130,14 +145,13 @@ const timer = () => {
 
 
 /**
- * 5. 카드 클릭 이벤트
+ * 5. 카드 뒤집기 게임 실행
  * @param {*} event
  * @return {*} cards 카드 배열
  * @description 카드를 순회하면서 클릭이벤트가 발생하면 두 카드를 비교하고 match되면 matchCard() 함수 실행.
  */
 const flipCard = (event) => {
     let clickedCard = event.target.closest('.card');
-
     // cardOne의 값이 비어있는 상태이며, clickPause가 false일 때만 실행
     if (cardOne !== clickedCard && !clickPause) {
         clickedCard.classList.add('show');
@@ -155,6 +169,7 @@ const flipCard = (event) => {
 };
 
 
+
 /**
  * 6. 카드 매칭
  * @param {one, two}
@@ -169,12 +184,14 @@ const matchCards = (one, two) => {
 }
 
 
+
 /**
  * 7-1. 카드 매칭 성공 시
  * @return {*} 초기화된 cardOne, cardTwo
  * @description Match된 카드는 클릭 이벤트를 제거하고 Count 1씩 증가.
  */
 function equals() {
+    // ? 이벤트를 cardOne, cardTwo 두 변수에 각각 주는 게 최선일까?
     cardOne.removeEventListener('click', flipCard);
     cardTwo.removeEventListener('click', flipCard);
     matchedCount++;
@@ -185,10 +202,9 @@ function equals() {
         endGame(cleared);
     }
 
-    // cardOne, cardTwo 초기화
-    cardOne = cardTwo = "";
-    return clickPause = false;
+    cardReset();
 }
+
 
 
 /**
@@ -198,6 +214,7 @@ function equals() {
  */
 function differs() {
     setTimeout(() => {
+        // ? 두 변수에 중복된 이벤트를 주다보니 코드가 지저분하다.
         // Match되지 않으면 shake Css Animation 0.4초 동안 실행
         cardOne.classList.add("shake");
         cardTwo.classList.add("shake");
@@ -207,12 +224,21 @@ function differs() {
         // 1초 후 shake, show class 제거해서 뒤집힌 카드로 원상복귀
         cardOne.classList.remove("shake", "show");
         cardTwo.classList.remove("shake", "show");
-
-        // cardOne, cardTwo 초기화
-        cardOne = cardTwo = "";
-        clickPause = false;
+        cardReset();
     }, 1000);
 }
+
+
+
+/**
+ * 7-3. 카드 초기화
+ * @description cardOne, cardTwo, clickPause 초기화
+ */
+const cardReset = () => {
+    cardOne = cardTwo = "";
+    return clickPause = false;
+}
+
 
 
 /**
@@ -222,39 +248,61 @@ function differs() {
  * @param {*} failed
 */
 const endGame = result => {
-    let resultMsg = $dialog.querySelector('h3 span');
-    let resultImg = $dialog.querySelector('h3 img');
+    let resultMsg = dialog.querySelector('h3 span');
+    let resultImg = dialog.querySelector('h3 img');
     resultImg.setAttribute('src', `../asset/img/${result.img}`);
     resultMsg.innerText = `Game ${result.msg}!`;
     result.sound.play();
     clearInterval(time);
-    $dialog.showModal();
+    dialog.showModal();
 }
 
 
-/**
- * 9. 게임 시작 함수
- * @description 화면이 로드되거나 사용자가 재시작 버튼을 눌렀을 때 render() 함수 실행.
-*/
-const render = () => {
-    randomCard();
-    setCard();
-    previewCard();
-    timer();
-}
-render();
-
 
 /**
- * 10. 카드 클릭 이벤트
+ * 9. 카드 클릭 이벤트
  * @param {*} event
  * @return {*} cards 카드 배열
- */
-cards.forEach(card => {
-    card.addEventListener('click', flipCard);
-})
+*/
+// 카드 클릭 이벤트
+const addCardClickEvent = () => {
+    // ? 변수를 중복해서 선언하는 게 최선일까? DOM으로 요소가 뿌려지기 전에 전역에서 선언하면 안 나옴...
+    cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('click', flipCard);
+    });
+};
 
-$restart.addEventListener('click', () => {
-    setTimeout(render, 1000);
+
+
+/**
+ * 10. 게임 시작 함수
+ * @description 화면이 로드되거나 사용자가 재시작 버튼을 눌렀을 때 game() 함수 실행.
+*/
+const game = () => {
+    // ? 이런식으로 랜더링을 함수로 빼는 게 최선일까?
+    shuffleCards();
+    renderCards();
+    showCards();
+    startTimer();
+    addCardClickEvent();
+}
+game();
+
+
+
+// 재시작 버튼 클릭 이벤트
+restart.addEventListener('click', () => {
     start.play();
+    cardReset();
+    game();
 });
+
+
+/**
+ * TodoList
+ * 현재 함수 순서 배치가 효율적인지 확인
+ * 변수를 효율적으로 선언 및 사용했는지 확인
+ * 함수를 효율적으로 분리해서 배치했는지 확인
+ * 함수를 효율적으로 작성했는지 확인
+ */
